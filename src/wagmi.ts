447007bc88f1a91fb3a108a296fd5ee363976835
mdr <matthew.drummond.ritch@gmail.com>
2025-02-15 13:51:12 -0500

    improve connectbutton. enable mobile

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,15 +1,37 @@
-import {
-  mainnet, base
-} from 'wagmi/chains'
+import { mainnet, base } from 'wagmi/chains'
 import { http, createConfig } from 'wagmi'
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+  rainbowWallet,
+  walletConnectWallet,
+  coinbaseWallet,
+  metaMaskWallet,
+
+} from '@rainbow-me/rainbowkit/wallets';
+
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [metaMaskWallet, rainbowWallet, walletConnectWallet, coinbaseWallet],
+    },
+  ],
+  {
+    appName: 'Facthound',
+    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_KEY??'',
+  }
+);
+
+
+
 
 
 export const config = createConfig({
-  chains: [
-    base,
-  ],
+  chains: [base],
+  connectors: connectors,
   transports: {
     [base.id]: http('https://base-mainnet.g.alchemy.com/v2/J5VbtLhS-VFrpXkxRWPX9XbNIOOcmI_K'),
-},
+  },
   ssr: true,
 });
